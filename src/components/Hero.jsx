db886c7f13706fc311d272712a1618ba4b766688
KILLERTIAN <omsharma050322@gmail.com>
2024-07-01 13:13:04 +0530

    links added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,22 +12,22 @@ function Hero() {
                     <div className="flex flex-row items-center justify-between rounded-md border-[1px] border-[#d5d9d9] overflow-hidden min-w-[310px] m-0">
                         <img className="h-[98px]  object-cover" src="https://m.media-amazon.com/images/G/31/img23/Beauty/PrimeCBcouponImage/Clothing.jpg" alt="clothing" />
                         <div className="flex flex-col items-start justify-center px-2 min-w-[224px]">
-                            <h2 className="text-[15px] ">Get $75 back</h2>
+                            <h2 className="text-[15px] ">Get &#8377;75 back</h2>
                             <h3 className="text-[13px] text-gray-700">using prepaid payment method</h3>
                             <div className="flex flex-row items-center justify-between w-full ">
-                                <h4 className="text-[10px] text-gray-400">Min Order: $250</h4>
-                                <button className="bg-[#ffd814] border-2 border-[#fcd200] py-1 px-4 rounded-sm text-[13px] min-w-[96px]">Collect</button>
+                                <h4 className="text-[10px] text-gray-400">Min Order: &#8377;250</h4>
+                                <a href="https://www.amazon.in/b/?_encoding=UTF8&node=6648217031&ref_=in_cat_halo_mweb_bau_fashion&pd_rd_w=PLS3d&content-id=amzn1.sym.447b2776-cd0a-4b23-8528-0c2793b0182c&pf_rd_p=447b2776-cd0a-4b23-8528-0c2793b0182c&pf_rd_r=E79MDRQC78BJQ88DYXF5&pd_rd_wg=PBJNN&pd_rd_r=134677ef-195a-4eca-8760-7a1df95a8e2d" className="flex items-center justify-center bg-[#ffd814] border-2 border-[#fcd200] py-1 px-4 rounded-sm text-[13px] min-w-[96px]">Collect</a>
                             </div>
                         </div>
                     </div>
                     <div className="flex flex-row items-center justify-between rounded-md border-[1px] border-[#d5d9d9] overflow-hidden min-w-[310px] m-0 ">
                         <img className="h-[98px]  object-cover" src="https://m.media-amazon.com/images/G/31/00/GameX/N2GL/beauty_Mini-carousal_176x200.jpg" alt="beauty" />
                         <div className="flex flex-col items-start justify-center px-2 min-w-[224px]">
-                            <h2 className="text-[15px] ">Get $75 back</h2>
+                            <h2 className="text-[15px] ">Get &#8377;75 back</h2>
                             <h3 className="text-[13px] text-gray-700">using prepaid payment method</h3>
                             <div className="flex flex-row items-center justify-between w-full ">
-                                <h4 className="text-[10px] text-gray-400">Min Order: $250</h4>
-                                <button className="border-[1px] border-[#d5d9d9] py-1 px-4 rounded-sm text-[13px] min-w-[96px] shadow">Redeem</button>
+                                <h4 className="text-[10px] text-gray-400">Min Order: &#8377;250</h4>
+                                <a className="border-[1px] border-[#d5d9d9] py-1 px-4 rounded-sm text-[13px] min-w-[96px] shadow flex items-center justify-center" href="https://www.amazon.in/b/?_encoding=UTF8&node=1355016031&ref_=in_cat_halo_mweb_bau_beauty&pd_rd_w=GitYX&content-id=amzn1.sym.447b2776-cd0a-4b23-8528-0c2793b0182c&pf_rd_p=447b2776-cd0a-4b23-8528-0c2793b0182c&pf_rd_r=TXQMR02X87AETP57WK97&pd_rd_wg=S1xzs&pd_rd_r=ba0480d2-1050-47ee-a822-5edbabfbe52a">Redeem</a>
                             </div>
                         </div>
                     </div>
@@ -49,7 +49,7 @@ function Hero() {
                         <div className="flex flex-col rounded-sm border-[2px] border-[#ffa41c] bg-slate-50 items-center justify-center w-full max-w-[150px]">
                             <img className="m-auto h-[56px]" src="https://m.media-amazon.com/images/S/engagement-central-media-prod-1/gamification/streak/sticker-256x256_QHV2GW.png" alt="action image" />
                             <h3 className="items-center font-semibold text-[11px] text-center">Collect <br /> sticker</h3>
-                            <button className="bg-[#ffa41c] text-[#0f1111] w-full text-[13px] p-[3px]">Start</button>
+                            <a href="https://www.amazon.in/b?node=1355016031" className="bg-[#ffa41c] text-[#0f1111] w-full text-[13px] p-[3px] items-center justify-center flex">Start</a>
                         </div>
                         <div className="flex flex-col rounded-sm border-[2px] border-[#ffa41c] bg-slate-50 items-center justify-center w-full max-w-[150px]">
                             <img className="m-auto h-[56px]" src="https://m.media-amazon.com/images/S/engagement-central-media-prod-1/gamification/streak/sticker-256x256_LF7TQP.png" alt="action image" />
@@ -63,7 +63,7 @@ function Hero() {
                         </div>
                         <div className="flex flex-col rounded-sm border-[2px] border-[#ffa41c] bg-slate-50 items-center justify-center w-full max-w-[150px] min-h-[117.7px]">
                             <img className="m-auto h-[34px]" src="https://m.media-amazon.com/images/S/engagement-central-media-prod-1/gamification/streak/streeks-_YXTJTA.png" alt="action image" />
-                            <span className="flex items-center justify-center bg-[#d13e3e] text-white text-[13px] px-1 rounded-lg">$60 back</span>
+                            <span className="flex items-center justify-center bg-[#d13e3e] text-white text-[13px] px-1 rounded-lg">&#8377;60 back</span>
                             <h3 className="items-center font-semibold text-[11px] text-center">Collect <br /> sticker</h3>
                             <button className="bg-gray-400 text-[#0f1111] w-full text-[13px] p-[3px]">Locked</button>
                         </div>
@@ -78,17 +78,31 @@ function Hero() {
             </div>
 
             {/* new section */}
-            <img className="py-7 px-3" src="https://images-eu.ssl-images-amazon.com/images/G/31/OHL_HSS/Without-powered-by_Banners-770x350spinjuly.jpg" alt="hoome" />
+            <img className="pt-5 px-3" src="https://res.cloudinary.com/djoebsejh/image/upload/v1719807749/d52gstjexhjukkuxqpcd.gif" alt="hoome" />
+            <a href="https://www.amazon.in/game/gPIE517/ref=fz_gblive_quiztime_lp_gPIE517/ref=mh_s9_cg_appgb_1a1_w?pf_rd_m=AT95IG9ONZD7S&pf_rd_s=mobile-hybrid-9&pf_rd_r=YWH72GMGYNBNY216AFMC&pf_rd_t=30901&pf_rd_p=bfe6b48f-c00c-4ab4-8de2-b96f8746c057&pf_rd_i=14351766031">
+                <img className="py-7 px-3" src="https://images-eu.ssl-images-amazon.com/images/G/31/OHL_HSS/Without-powered-by_Banners-770x350spinjuly.jpg" alt="hoome" />
+
+            </a>
 
             <div className="flex flex-col p-3 items-center justify-center">
                 <img src="https://images-eu.ssl-images-amazon.com/images/G/31/img17/Mobile/Engagement/ASMs/Yamini/qc_19th_june24/FZ_Week25-QC_01.jpg" alt="spin head" />
                 <div className="flex flex-row w-full items-center justify-center">
-                    <img className="w-[50%]" src="https://images-eu.ssl-images-amazon.com/images/G/31/img17/Mobile/Engagement/ASMs/Yamini/qc_19th_june24/FZ_Week25-QC_02.gif" alt="1" />
-                    <img className="w-[50%]" src="https://images-eu.ssl-images-amazon.com/images/G/31/img17/Mobile/Engagement/ASMs/Yamini/qc_19th_june24/FZ_Week25-QC_03.gif" alt="2" />
+                    <a className="w-[50%]" href="https://www.amazon.in/rewards/checkoutCoupons?uuid=NX9GWE" >
+                        <img src="https://images-eu.ssl-images-amazon.com/images/G/31/img17/Mobile/Engagement/ASMs/Yamini/qc_19th_june24/FZ_Week25-QC_02.gif" alt="1" />
+
+                    </a>
+                    <a className="w-[50%]" href="https://www.amazon.in/rewards/checkoutCoupons?uuid=LNB376" >
+                        <img src="https://images-eu.ssl-images-amazon.com/images/G/31/img17/Mobile/Engagement/ASMs/Yamini/qc_19th_june24/FZ_Week25-QC_03.gif" alt="2" />
+
+                    </a>
                 </div>
                 <div className="flex flex-row w-full items-center justify-center">
-                    <img className="w-[50%]" src="https://images-eu.ssl-images-amazon.com/images/G/31/img17/Mobile/Engagement/ASMs/Yamini/qc_19thjune/FZ_Week25-QC_04.jpg" alt="1" />
-                    <img className="w-[50%]" src="https://images-eu.ssl-images-amazon.com/images/G/31/img17/Mobile/Engagement/ASMs/Yamini/qc_19thjune/FZ_Week25-QC_05.jpg" alt="2" />
+                    <a className="w-[50%]" href="https://www.amazon.in/rewards/checkoutCoupons?uuid=NX9GWE" >
+                        <img src="https://images-eu.ssl-images-amazon.com/images/G/31/img17/Mobile/Engagement/ASMs/Yamini/qc_19thjune/FZ_Week25-QC_04.jpg" alt="1" />
+                    </a>
+                    <a className="w-[50%]" href="https://www.amazon.in/rewards/checkoutCoupons?uuid=LNB376" >
+                    <img src="https://images-eu.ssl-images-amazon.com/images/G/31/img17/Mobile/Engagement/ASMs/Yamini/qc_19thjune/FZ_Week25-QC_05.jpg" alt="2" />
+                    </a>
                 </div>
             </div>
             <img src="https://images-eu.ssl-images-amazon.com/images/G/31/img22/WLA/2024/GAMEX/SPIN_WIN/Ptron/ZENBUDS_PRO1/D132814684_IN_WLA_Ptron_LaunchDesignSIM_750x350.jpg" alt="a" className="py-2 px-3" />
@@ -96,7 +110,7 @@ function Hero() {
             <img src="https://images-eu.ssl-images-amazon.com/images/G/31/img17/Mobile/Engagement/ASMs/Amritesh/Supradyn_Play_and_Win_copy_Rs_80.jpg" alt="c" className="py-2 px-3" />
             <img src="https://images-eu.ssl-images-amazon.com/images/G/31/img24/Wireless/tdhruvko/Vivo/XFold3/Gamex/D134821330_IN_WLD_vivo_XFold3Pro_launch_Gamex_750x350.jpg" alt="d" className="py-2 px-3" />
             <img src="https://images-eu.ssl-images-amazon.com/images/G/31/IN_Devices/BAU_GameX/With_Play_Now_750x350.png" alt="e" className="py-2 px-3" />
-            
+
             {/* Divider */}
             <div className="h-[1px] bg-slate-300 w-full my-4"></div>
 
@@ -125,7 +139,7 @@ function Hero() {
                 <img src="https://images-eu.ssl-images-amazon.com/images/G/31/img17/Mobile/Engagement/ASMs/Amritesh/FZ-Coins-Dec-2022/faq/update/FAQ_3.png" alt="faq" />
                 <img src="https://images-eu.ssl-images-amazon.com/images/G/31/img17/Mobile/Engagement/ASMs/Amritesh/FZ-Coins-Dec-2022/faq/update/FAQ_4.png" alt="faq" />
             </div>
-            
+
         </div>
     )
 }
